73e9465fa886df3a0584ee133c21d1daf8555064
OJ Kwon <1210596+kwonoj@users.noreply.github.com>
2025-04-17 09:43:51 -0700

    feat(index): initial test

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import { serve } from "@hono/node-server";
+import { serveStatic } from "@hono/node-server/serve-static";
 import { Hono } from "hono";
+import { join } from "path";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
+// Serve the index.html file at the root route
+app.get("/", serveStatic({ path: join(process.cwd(), "public/index.html") }));
+
+// Serve other static files from the public directory
+app.use("/*", serveStatic({ root: "./public" }));
 
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 console.log(`Server is running on port ${port}`);
